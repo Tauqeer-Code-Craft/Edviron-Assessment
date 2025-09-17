b0b0e63f6a70271b70a9d003a296ec675f894967
Tauqeer-Code-Craft <143533468+Tauqeer-Code-Craft@users.noreply.github.com>
2025-09-17 20:00:51 +0530

    Update app.js

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -14,7 +14,7 @@ const app = express();
 // Middleware
 app.use(helmet());  // headers will be secured
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://edviron-assessment-eosin.vercel.app",
     credentials: true,
 }));
 app.use(express.json({
